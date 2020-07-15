d417f1f44fcdea03d5a437e63e9dfa2da43ae559
Seif Habbachi <shabbach@student.42.fr>
2020-07-15 13:35:54 +0200

    reset

--- ApiConfig/mongoose.js
+++
@@ -1,4 +0,0 @@
-let mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/CO-PRO-VISIT', { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
-
-module.exports = mongoose;
