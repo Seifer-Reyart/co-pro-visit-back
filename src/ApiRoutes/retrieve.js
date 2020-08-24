1f0a043b5079358ed68e25299dc99a106d4435b6
Seifer <shabbach@student.42.fr>
2020-08-24 13:18:52 +0200

    get Syndics list

--- src/ApiRoutes/retrieve.js
+++ src/ApiRoutes/retrieve.js
@@ -25,7 +25,7 @@ let router = express.Router();
  * @returns {object} 200 - {success: true, syndics: array of syndics}
  * @returns {Error}  400 - {success: false, message: error system log from mongoose}
  * @returns {Error}  401 - si dans token, role !== admin ou courtier ou prestataire  {success: false, message: 'accès interdit'}
- * @returns {Error}  403 - si email existe  {success: false, message: "ce compte n'existe pas!"}
+ * @returns {Error}  403 - si le compte n'est pas enregistré {success: false, message: "ce compte n'existe pas!"}
  * @returns {Error}  404 - si email existe  {success: false, message: 'aucun syndic enregistré'}
  * @produces application/json
  * @consumes application/json
