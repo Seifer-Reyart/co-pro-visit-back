b45179f33f9a28044b03cabd5b3bff21f386fb53
Seifer <shabbach@student.42.fr>
2020-11-18 02:02:43 +0100

    logs

--- src/ApiControllers/gestion.js
+++ src/ApiControllers/gestion.js
@@ -414,12 +414,12 @@ let assignerCourtierToSyndic = async (req, res) => {
 }
 
 let assignerPrestataireToSyndic = async (req, res) => {
+    const {prestataireId, syndics, option} = req.body;
     if (req.user.role !== 'admin')
         res.status(401).send({success: false, message: 'accès interdit'});
     else if (!prestataireId || !syndics)
         res.status(403).send({success: false, message: 'syndicId et prestataireId requis'});
     else {
-        const {prestataireId, syndics, option} = req.body;
         let errorSyndic = [];
         let errorPresta = [];
         let successId = [];
